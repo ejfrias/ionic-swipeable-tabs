dc69038206e8ca94372f30c7538dc791aed8a274
EJ Frias <ejsfrias@gmail.com>
2018-06-14 12:04:59 +0800

    Update swipeable-tabs.ts
    Added comments and removed all unneeded the code

--- src/components/swipeable-tabs/swipeable-tabs.ts
+++ src/components/swipeable-tabs/swipeable-tabs.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnDestroy, ElementRef, Renderer2, ContentChild, Output, EventEmitter, HostListener } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, ElementRef, Renderer2, ContentChild, HostListener } from '@angular/core';
 import { Slides } from 'ionic-angular';
 
 @Component({
@@ -7,10 +7,11 @@ import { Slides } from 'ionic-angular';
 })
 export class SwipeableTabsComponent implements OnInit, OnDestroy {
 	@Input('tabs') tabs: Array<string>;
-	@Input('activeIndex') activeIndex: number;
+	@Input('activeIndex') activeIndex: number = 0;
 	@Input('slidesFullHeight') slidesFullHeight: any = true;
 	@ContentChild(Slides) slides: Slides;
 	@HostListener('window:resize') onWindowResize() {
+		//update the active tab indicator when the window has been resized
 		this.updateTabWidth();
 	}
 
@@ -20,110 +21,79 @@ export class SwipeableTabsComponent implements OnInit, OnDestroy {
 	public tabWidth: number;
 	public slideWidth: number;
 	public slideContainer: HTMLElement;
-	public slideDrag$;
 	public slideChanged$;
-	public slideMouseUp$;
-	public slideTouchEnd$;
 
 	constructor(
 		public el: ElementRef,
 		public renderer: Renderer2
-	) {
-		
-	}
+	) {}
 
 	ngOnInit() {}
 
+	/**
+	 * All input will be initilized after this function so
+	 * we start setting up everything here
+	 */
 	ngAfterViewInit() {
-		if (!this.activeIndex) {
-			this.activeIndex = 0;
-		}
-		
+		//set the height of the slides
 		if (String(this.slidesFullHeight).toLowerCase() == 'false') {
 			this.renderer.addClass(this.el.nativeElement, 'slides-height-auto');
 		}
 	
+		//get the needed elements
 		this.tabsHeader = this.el.nativeElement.querySelector('.swipeable-tabs-header');
 		this.activeTabIndicator = this.el.nativeElement.querySelector('.active-tab-indicator');
 		this.slideContainer = this.el.nativeElement.querySelector('.swiper-container');
 
-		//set active tab
-		if (this.activeIndex != 0) {
-			this.renderer.addClass(this.activeTabIndicator, 'no-transition'); //remove the animation
-
-			setTimeout(() => {
-				this.slideTo(this.activeIndex, 0);
-			}, 20);
-
-			//bring back the animation
-			setTimeout(() => {
-				this.renderer.removeClass(this.activeTabIndicator, 'no-transition');
-			}, 200);
-		}
-
-		//get the tabs element
+		//get each tab element
 		this.getTabs();
 
-		//set the tab width
+		//set the active tab indicator width
 		this.updateTabWidth();
 
-		//listen to slide change event
+		//set active tab
+		if (this.activeIndex != 0) {
+			//make sure we run the commands in order
+			(async () => {
+				await this.renderer.addClass(this.activeTabIndicator, 'no-transition'); //remove the animation
+				await new Promise(resolve => {
+					setTimeout(() => {
+						this.slideTo(this.activeIndex, 0); //set initial slide
+						resolve(true);
+					}, 20);
+					
+				});
+
+				//bring back the animation
+				this.renderer.removeClass(this.activeTabIndicator, 'no-transition');
+			})();
+		}
+
+		//update the active tab indicator when the slide changes
 		this.slideChanged$ = this.slides.ionSlideWillChange.subscribe(event => {
-			//update the active tab indicator when the slide changes
 			this.setActiveTab(this.slides.getActiveIndex());
 		});
-		
-		/* TODO: update the active tab indicator position while swiping
-		//listen to slide drag event
-		this.slideDrag$ = this.slides.ionSlideDrag.subscribe(event => {
-			let container = event.container;
-			let translated = [];
-			let scrollPos = this.getPosX(container);
-			let speed = this.tabWidth / container.offsetWidth;
-			let indicatorPos = speed * Math.abs(scrollPos);
-
-			this.updateTabIndicatorPosX(indicatorPos);
-		});
-
-		//listen to mouse up event to revert back to original position
-		this.slideMouseUp$ = this.renderer.listen(this.slideContainer, 'mouseup', e => {
-			let activeSlideIndex = this.slides.getActiveIndex();
-			let containerWidth = this.slideContainer.offsetWidth;
-			let posX = this.getPosX(this.slideContainer); // + (activeSlideIndex * containerWidth) + this.tabWidth;
-
-			if (posX >= 0) {
-				this.updateTabIndicatorPosX(posX);
-			}
-		});
-
-		//listen to touchend event to revert back to original position
-		this.slideTouchEnd$ = this.renderer.listen(this.slideContainer, 'touchend', e => {
-			let activeSlideIndex = this.slides.getActiveIndex();
-			let containerWidth = this.slideContainer.offsetWidth;
-			let posX = this.getPosX(this.slideContainer); // + (activeSlideIndex * containerWidth) + this.tabWidth;
-
-			if (posX >= 0) {
-				this.updateTabIndicatorPosX(posX);
-			}
-		});
-		*/
 	}
 
+	/**
+	 * Destroy subscriptions
+	 */
 	ngOnDestroy() {
 		this.slideChanged$.unsubscribe();
-		/*
-		this.slideDrag$.unsubscribe();
-		this.slideMouseUp$.unsubscribe();
-		this.slideTouchEnd$.unsubscribe();
-		*/
 	}
 
+	/**
+	 * Get all tabs
+	 */
 	getTabs() {
 		setTimeout(() => {
 			this.tabsEl = this.el.nativeElement.querySelectorAll('.col');
 		});
 	}
 
+	/**
+	 * Set the active tab indicator same as the tab
+	 */
 	updateTabWidth() {
 		setTimeout(() => {
 			this.tabWidth = this.el.nativeElement.querySelector('.col').offsetWidth;
@@ -131,13 +101,24 @@ export class SwipeableTabsComponent implements OnInit, OnDestroy {
 		});
 	}
 
+	/**
+	 * Set slide active index and transition to the specific slide
+	 * 
+	 * @param index (number) - the index number of the slide (starts from 0)
+	 * @param duration (number) - duration of the transition (in ms)
+	 */
 	slideTo(index, duration: number = 500) {
 		this.activeIndex = index;
 		this.slides.slideTo(index, duration);
 	}
 
+	/**
+	 * Set the active tab and move the active tab indicator
+	 * 
+	 * @param index (number) - the index number of the tab
+	 */
 	setActiveTab(index) {
-		let activeTab = this.tabsEl[index];
+		let activeTab = typeof this.tabsEl[index] != 'undefined' ? this.tabsEl[index] : null;
 
 		if (activeTab) {
 			this.activeIndex = index;
@@ -145,25 +126,12 @@ export class SwipeableTabsComponent implements OnInit, OnDestroy {
 		}
 	}
 
+	/**
+	 * Update the position of the active tab indicator
+	 * 
+	 * @param xPos (number) - the horizontal position
+	 */
 	updateTabIndicatorPosX(xPos: number) {
 		this.renderer.setStyle(this.activeTabIndicator, 'left', xPos + 'px');
-	}
-
-	getPosX(el: HTMLElement) {
-		let scrollPos = 0;
-		let translated = [];
-		let firstChild = <HTMLElement>el.firstChild;
-
-		if (firstChild.style.transform) {
-			translated = firstChild.style.transform.split(/\w+\(|\);?/);
-			
-			//move the active tab indicator when the slide has been dragged
-			if (translated[1] && translated[1].length) {
-				translated = translated[1].split(/,\s?/g);
-				scrollPos = parseInt(translated[0]);
-			}
-		}
-
-		return scrollPos;
 	}
 }
